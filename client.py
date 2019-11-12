7426f0c752609b8da36c269dc47415067490ab3b
Jon Craton <jncraton@gmail.com>
2019-11-12 13:48:21 -0500

    Use var for server and port

--- client.py
+++ client.py
@@ -2,11 +2,14 @@
 import socket
 import os
 
+server = '127.0.0.1'
+port = 2000
+
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('127.0.0.1', 2000))
+        s.connect((server, port))
     
         s.sendall('get'.encode('utf8'))
         print(s.recv(4096).decode('utf8'))
@@ -16,5 +19,5 @@ while True:
 
     if new_msg:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(('127.0.0.1', 2000))
+            s.connect((server, port))
             s.sendall(f'send{new_msg}\n'.encode('utf8'))
